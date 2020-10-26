12692badd43a282a08763b5048b2805a4651f6af
ben20262 <ben20262@aim.com>
2020-10-25 21:31:44 -0400

    added addSellers

--- src/containers/SellerContainer.js
+++ src/containers/SellerContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import { addSellers } from '../actions/index'
  
 class SellerContainer extends Component {
     render() { 
@@ -12,11 +13,11 @@ class SellerContainer extends Component {
 }
  
 const mapStateToProps = state => {
-    
+    sellers: state.sellers
 }
 
 const mapDispatchToProps = dispatch => {
-
+    addSellers: () => dispatch(addSellers())
 }
  
 export default connect(mapStateToProps, mapDispatchToProps)(SellerContainer);
