56365d0ac671af425c1b8e69bef66f98422fbeda
ben20262 <ben20262@aim.com>
2020-10-25 21:48:52 -0400

    added seller components

---
+++ src/components/seller/Seller.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Seller extends Component {
+
+
+    render() {
+        return (
+            <div>
+                Seller
+            </div>
+        )
+    }
+}
+
+export default Seller
---
+++ src/components/seller/Sellers.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sellers = props => {
+    return <div>
+        Sellers
+    </div>
+}
+
+export default Sellers
