ccaee79591e27fafc762347b2426e8c012084689
ben20262 <ben20262@aim.com>
2020-11-01 21:38:25 -0500

    added props

--- src/components/seller/Sellers.js
+++ src/components/seller/Sellers.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import Seller from './Seller'
 
 const Sellers = props => {
     return <div>
-        Sellers
+        {props.sellers.map(seller => <Seller seller={seller}/>)}
     </div>
 }
 
