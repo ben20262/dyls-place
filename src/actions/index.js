38f1c1b6581d26abd821036b248b1fd7f86281c3
ben20262 <ben20262@aim.com>
2020-11-01 21:50:06 -0500

    added fetchCars

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,3 +5,10 @@ export const addCars = cars =>  {
 export const addSellers = sellers => {
 
 }
+
+export const fetchCars = () => {
+    return dispatchEvent({type: 'LOADING_CARS'})
+    fetch(`${apiURL}/cars`)
+    .then(resp => resp.json())
+    .then(cars => dispatchEvent({type: 'ADD_CARS', cars}))
+}
