ff0b1792d038a20a47c663b2ec9c731ed5d3688b
ben20262 <ben20262@aim.com>
2020-11-01 21:38:35 -0500

    added render function

--- src/components/seller/Seller.js
+++ src/components/seller/Seller.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Seller extends Component {
+const Seller = props => {
 
-
-    render() {
-        return (
-            <div>
-                Seller
-            </div>
-        )
-    }
+    let cars = props.seller.cars.map(car => <li>{car.title}</li>) //Links
+    return (
+        <div>
+            <h3>{Seller.name}</h3>
+            <ul>
+                {cars}
+            </ul>
+        </div>
+    )
 }
 
 export default Seller
