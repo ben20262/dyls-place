222572c3fd0f6e429eb5b2c30a7a12004a010c2a
ben20262 <ben20262@aim.com>
2020-11-06 21:18:45 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React from 'react'
+import { 
+  BrowserRouter  as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 import './App.css';
 
+
+import SellerContainer from './containers/SellerContainer'
+import CarContainer from './containers/CarContainer'
+
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	return (
+		<div className="App">
+			<nav>
+				<ul>
+					<li><Link to='/'>Cars</Link></li>
+					<li><Link to='/Sellers'>Sellers</Link></li>
+				</ul>
+			</nav>
+			<Switch>
+				<Route path='/sellers'>
+					<SellerContainer />
+				</Route>
+				<Route path='/'>
+					<CarContainer />
+				</Route>
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
