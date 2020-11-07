54f28e223bf4f95ccaf75be4158f5af46da1cf3a
ben20262 <ben20262@aim.com>
2020-11-06 21:23:46 -0500

    added more cases

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,24 @@
-export default function rootReducer(state = {cars: [], sellers: []}, action) {
+export default function rootReducer(state = {cars: [], sellers: [], loading: true}, action) {
     switch (action.type) {
         case('ADD_CARS'):
-            return state
+            return {
+                cars: action.cars,
+                sellers: action.sellers,
+                loading: false
+            }
+        case('FETCH_CARS'):
+            return {
+                ...state,
+                loading: true
+            }
+        case('ADD_CAR'):
+            return {
+                ...state
+            }
+        case('ADD_SELLER'):
+            return {
+                ...state
+            }
         default:
             return state
     }
